d92f1fd7c8e93e9b8f0c8dd9c19c29c958f4cf05
nverchev <nicolas.vercheval@gmail.com>
2026-01-12 19:48:04 +0100

    Added TensorBoardLogReconstruction

--- src/hooks.py
+++ src/hooks.py
@@ -74,12 +74,7 @@ class DiscreteSpaceOptimizer:
 
 
 class WandbLogReconstruction:
-    """Log sample reconstructions to Weights & Biases during training.
-
-    This hook visualizes the original input samples and their reconstructions
-    in 3D using Weights & Biases logging functionality. It helps monitor the
-    quality of the autoencoder's reconstruction capabilities.
-    """
+    """Log sample reconstructions to Weights & Biases during training."""
 
     def __init__(self, dataset: data.Dataset[tuple[Inputs, Targets]], num_samples: int = 1):
         """Initialize the logging hook.
@@ -114,3 +109,46 @@ class WandbLogReconstruction:
         # Log reconstructions as 3D objects
         for i, recon in enumerate(outputs.recon.detach().cpu().numpy()):
             self.run.log({f'Recon {i}': wandb.Object3D(recon)})
+
+
+class TensorBoardLogReconstruction:
+    """Log sample reconstructions to Tensorboard during training."""
+
+    def __init__(self, dataset: data.Dataset[tuple[Inputs, Targets]], num_samples: int = 1):
+        """Initialize the logging hook.
+
+        Args:
+            dataset: Dataset containing input samples to visualize.
+            num_samples: Number of samples to log in each iteration.
+        """
+        from drytorch.trackers.tensorboard import TensorBoard
+
+        self._dataset = dataset
+        self._num_samples = num_samples
+        self.writer = TensorBoard.get_current().writer
+        inputs, targets = take_from_dataset(dataset, num_samples)
+        for i, (input_, label) in enumerate(zip(inputs.cloud, targets.label)):
+            self.writer.add_mesh(
+                tag=f'Sample {i} with label: {label.item()}',
+                vertices=input_.unsqueeze(0),
+                global_step=0,
+            )
+        return
+
+    def __call__(self, trainer: p.TrainerProtocol[Inputs, Targets, Outputs]) -> None:
+        """Log reconstructed samples to Tensorboard.
+
+        Args:
+            trainer: Training protocol containing the model to evaluate.
+        """
+        model = trainer.model
+
+        inputs, targets = take_from_dataset(self._dataset, self._num_samples, device=model.device)
+        outputs = model(inputs)
+
+        for i, recon in enumerate(outputs.recon.detach().cpu()):
+            self.writer.add_mesh(
+                tag=f'Recon {i}',
+                vertices=recon.unsqueeze(0),
+                global_step=0,
+            )
