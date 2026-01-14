bca5af60ffdee7deddec735f6f4e88ada5c0925a
nverchev <nicolas.vercheval@gmail.com>
2026-01-14 14:40:04 +0100

    created w_decoders.py and w_encoders.py

--- src/module/__init__.py
+++ src/module/__init__.py
@@ -1,13 +1,15 @@
 """Module package."""
 
 from src.module.autoencoders import VQVAE, CounterfactualVQVAE, get_autoencoder
-from src.module.w_decoders import get_w_decoder
-from src.module.w_encoders import get_w_encoder
-from src.module.w_autoencoder import CounterfactualWAutoEncoder
 from src.module.decoders import get_decoder
 from src.module.encoders import get_encoder
+from src.module.classifier import DGCNN
+from src.module.w_autoencoder import CounterfactualWAutoEncoder
+from src.module.w_decoders import get_w_decoder
+from src.module.w_encoders import get_w_encoder
 
 __all__ = [
+    'DGCNN',
     'VQVAE',
     'CounterfactualVQVAE',
     'CounterfactualWAutoEncoder',
