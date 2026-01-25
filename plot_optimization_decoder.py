59378d9ae51354a15abbf8024254e597f4757322
nverchev <nicolas.vercheval@gmail.com>
2026-01-25 15:33:54 +0100

    fixed study name

--- plot_optimization_decoder.py
+++ plot_optimization_decoder.py
@@ -12,7 +12,7 @@ from src.utils.tuning import get_study_name, visualize_study
 @hydra.main(version_base=None, config_path=ConfigPath.TUNING_AUTOENCODER.absolute(), config_name='defaults')
 def tune(tune_cfg: DictConfig):
     """Set up the study and launch the optimization."""
-    study_name = get_study_name(tune_cfg.tune.study_name, tune_cfg.overrides[1:])
+    study_name = get_study_name(tune_cfg.tune.study_name, tune_cfg.overrides)
     study = optuna.load_study(study_name=study_name, storage=tune_cfg.storage)
     visualize_study(study, tune_cfg.renderer)
     return
